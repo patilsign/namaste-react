cdf2e65aa89dac9ddc3e80b1c9264068bfde51dd
patilsign <patilsign108@gmail.com>
2024-12-04 02:05:57 +0530

    igniting app with install parcel,react,react-dom

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
 const heading = React.createElement("h1", {}, "Hello from React JS");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
 
 const div = React.createElement("div", { id: "parent" }, [
      React.createElement("div", { id: "child1" }, [
-          React.createElement("h1", {}, "Hi...Child 1  I'm h1 Tag"),
+          React.createElement("h1", {}, "Akshay"),
           React.createElement("h2", {}, "Hi... Child 1 I'm h2 Tag")
      ]
      ), React.createElement("div", { id: "child2" }, [
-          React.createElement("h1", {}, "Hi...Child 2 I'm h1 Tag"),
+          React.createElement("h1", {}, "Saini"),
           React.createElement("h2", {}, "Hi...Child 2 I'm h2 Tag")
      ]
      )
